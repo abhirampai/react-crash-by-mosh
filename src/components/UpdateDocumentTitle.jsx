5d7de88e08191d57e22f9126891db3b7157f650a
Pai026 <abhirampai1999@gmail.com>
2021-08-05 09:21:32 +0530

    conditional rendering using useEffect

--- src/components/UpdateDocumentTitle.jsx
+++ src/components/UpdateDocumentTitle.jsx
@@ -2,16 +2,25 @@ import React, { useState, useEffect } from "react";
 
 const UpdateDocumentTitle = () => {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
   useEffect(() => {
+    console.log("updated document title");
     document.title = `Button Clicked ${count} Times`;
-  });
+  },[count]);
   return (
-    <button
-      onClick={() => setCount(count + 1)}
-      className="btn btn-primary btn-sm"
-    >
-      Button Clicked {count} Times
-    </button>
+    <div>
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <button
+        onClick={() => setCount(count + 1)}
+        className="btn btn-primary btn-sm"
+      >
+        Button Clicked {count} Times
+      </button>
+    </div>
   );
 };
 
